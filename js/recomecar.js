78873c3480edd12df5e31496bbab56080c83d4a4
marianamachadox <marianalmachado09@gmail.com>
2021-08-18 20:51:56 -0300

    ajusta link js de recomecar

--- js/recomecar.js
+++ js/recomecar.js
@@ -1,10 +1,10 @@
 function start() {
-    var destino = prompt("Você gostaria de recomeçar?\n1-Sim\n1-Não");
+    var destino = prompt("Você gostaria de recomeçar?\n1-Sim\n2-Não");
     if(destino == 1){
-        window.location.href = "./idex.html";
+        window.location.href = "../index.html";
     }
     else if(destino == 2){
-        window.location.href = "Poxa, tudo bem? :( Te vejo na próxima, então. Valeu por lutar até aqui!";
+        alert("Poxa, tudo bem! :( Te vejo na próxima, então. Valeu por lutar até aqui!");
     }
     else{
         alert("Por favor, para continuar o jogo você deverá fazer uma escolha válida!");

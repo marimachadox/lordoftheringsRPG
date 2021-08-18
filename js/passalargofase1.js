74d7f1412c2f7de4f11b67bce1300a35d3356f18
marianamachadox <marianalmachado09@gmail.com>
2021-08-18 20:45:39 -0300

    corrige grafia

--- js/passalargofase1.js
+++ js/passalargofase1.js
@@ -1,5 +1,5 @@
 function start() {
-    var destino = prompt("1-Como guardião, oferece ao Hobbit e seus amigos, sua ajuda e proteção\n2-Como herdeiro direto do reino e do homem que derretou Sauron, você exige que o Hobbit lhe dê a posse do anel\n3-Você não se revela, nem oferece ajuda, fica observando o hobbit de longe esperando-o vacilar para pegar o anel");
+    var destino = prompt("1-Como guardião, oferece ao Hobbit e seus amigos, sua ajuda e proteção\n2-Como herdeiro direto do reino e do homem que derrotou Sauron, você exige que o Hobbit lhe dê a posse do anel\n3-Você não se revela, nem oferece ajuda, fica observando o hobbit de longe esperando-o vacilar para pegar o anel");
     if(destino == 2 || destino == 3){
         window.location.href = "../html/gameover.html";
     }

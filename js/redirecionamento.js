822dfbe54bbddf0d72554c75f7855f2336ac873f
marianamachadox <marianalmachado09@gmail.com>
2021-08-17 21:37:47 -0300

    modifica a seleção de personagens

--- js/redirecionamento.js
+++ js/redirecionamento.js
@@ -1,7 +1,37 @@
 function startfrodo() {
-    window.location.href = "./html/frodo1.html";
+    var escolha = prompt("Tem certeza da sua escolha?\n1-Para sim\n2-Para não")
+    if (+escolha === 1) {
+        window.location.href = "./html/frodo1.html";
+    }
+    else if (+escolha === 2) {
+        alert("Ok, você pode selecionar outro personagem!")
+    }
+    else {
+        alert("Por favor, insira uma numeração válida! :P")
+    }
 }
-
 function startgandalf() {
-    window.location.href = "./html/gandalf1.html";
+    var escolha = prompt("Tem certeza da sua escolha?\n1-Para sim\n2-Para não")
+    if (+escolha === 1) {
+        window.location.href = "./html/gandalf1.html";
+    }
+    else if (+escolha === 2) {
+        alert("Ok, você pode selecionar outro personagem!")
+    }
+    else {
+        alert("Por favor, insira uma numeração válida! :P")
+    }
+
+}
+function startstreed() {
+    var escolha = prompt("Tem certeza da sua escolha?\n1-Para sim\n2-Para não")
+    if (+escolha === 1) {
+        window.location.href = "./html/passolargo1.html";
+    }
+    else if (+escolha === 2) {
+        alert("Ok, você pode selecionar outro personagem!")
+    }
+    else {
+        alert("Por favor, insira uma numeração válida! :P")
+    }
 }

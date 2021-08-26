fde26dd65de93e8ac4170c2295f364f785e08323
marianamachadox <marianalmachado09@gmail.com>
2021-08-26 17:15:32 -0300

    corrige fase final gandalf

--- js/gandalffase4.js
+++ js/gandalffase4.js
@@ -1,7 +1,7 @@
 function start() {
     var destino = prompt("1-Ficar e usar toda sua força e habilidade para lutar, sozinho\n2-Tentar fugir com outros, até que que o demônio de fogo os perca de vista\n3-Sabe que não tem força suficiente para enfrentá-lo sozinho, então pede que frodo lhe empreste o anel para que você tenha chance de lutar e igual para igual");
     if (destino == 1) {
-        window.location.href = "../html/gandalfinal.html";
+        window.location.href = "../html/gandalffinal.html";
     }
     else if (destino == 2 || destino == 3) {
         window.location.href = "../html/gameover.html";
